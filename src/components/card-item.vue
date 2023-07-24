79b81f5b2dabe8e5ed53cbe86d19adcc25b2ed18
klay <ads1029384756@gmail.com>
2023-07-24 13:09:03 +0800

    images

--- src/components/card-item.vue
+++ src/components/card-item.vue
@@ -3,7 +3,7 @@ const props = defineProps({image:String, index: Number})
 </script>
 <template>
   <div class="card mb-4">
-    <img :src="'/src/assets/images/'+props.image+'.jpg'" class="card-img-top" alt="product" />
+    <img :src="'/images/'+props.image+'.jpg'" class="card-img-top" alt="product" />
     <div class="card-body">
       <h5 class="card-title">{{ `美甲服務${props.index}` }}</h5>
       <p class="card-text">
