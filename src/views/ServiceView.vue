7f64b9eb3abff223354a6b0523b3bb7f146db433
violet7755 <violet7755@gmail.com>
2023-09-17 02:52:08 +0800

    renew service

--- src/views/ServiceView.vue
+++ src/views/ServiceView.vue
@@ -6,25 +6,15 @@
     <h2 class="text-center py-96 text-white">服務項目</h2>
   </div>
   <!-- main section -->
-<<<<<<< HEAD
   <div class="container pb-24">
     <div class="row">
       <!-- 1 -->
       <div class="col-lg-6 d-none d-lg-block">
         <img src="/images/product-4.jpg"
-=======
-  <div class="container">
-    <div class="row">
-      <!-- 1 -->
-      <div class="col-lg-6 d-none d-lg-block">
-        <img
-          src="https://lh3.googleusercontent.com/CpUNKN_2tD8xM8CIU1RRuSgzsJ4VDJ7NNVRh53pbYrGUJqXnYSW_BIjGaWLh91gTnru6l6NQ1VsEvURfR5w6uMYdyKd8Ujlv5gRya1YnPnz3nf4AjnyvVxnblGqXJBDbYMVKEo-h7A=w2400"
->>>>>>> origin/main
           alt=""
         />
       </div>
       <div
-<<<<<<< HEAD
         class="col-12 col-lg-6 p-16 px-lg-32 d-flex flex-column justify-content-between mb-32 pic-1"
         style="height: 500px"
       >
@@ -34,21 +24,10 @@
 其實沾粉指甲在歐美國家已經有相當多的品牌可以選擇，影片中使用的是CUCCIO微晶粉，於歐美國家相當熱門，它的顆粒僅有原本水晶粉的十分之一細，卻擁有水晶指甲一般的硬度、也可塑形。很多人不太能接受水晶指甲使用的溶劑味道，甚至會頭暈或過敏。而微晶粉保有水晶指甲的特點，除了使用上不會有刺激氣味產生，也不需要像凝膠指甲需要搭配美甲燈才能操作，只需要在真指甲塗上膠水後沾粉，使用上更方便、快速、簡單，熟練的美甲師30-45分鐘即可完成在真甲上的操作，延甲也只需要60分鐘。更重要的是，使用的膠水中含有泛酸鈣與維生素E能夠幫助保持指甲的健康，真心推薦給軟甲、脆弱甲、咬指甲或者是做凝膠總是覺得燙手的人。</p>
         </div>
         <button type="button" class="mb-md-40 btn btn-lg btn-outline-primary align-self-end">MORE</button>
-=======
-        class="col-12 col-lg-6 p-16 d-flex flex-column justify-content-between mb-32 pic-1"
-        style="height: 500px"
-      >
-        <div>
-          <h3 class="">沾粉指甲</h3>
-          <p class="">123456</p>
-        </div>
-        <button type="button" class="btn btn-lg btn-outline-primary align-self-end">MORE</button>
->>>>>>> origin/main
       </div>
       <!-- 1 -->
       <!-- 2 -->
       <div
-<<<<<<< HEAD
         class="col-12 col-lg-6 p-16 px-lg-32 d-flex flex-column justify-content-between mb-32 pic-2"
         style="height: 500px"
       >
@@ -60,37 +39,17 @@
       </div>
       <div class="col-lg-6 d-none d-lg-block">
         <img class="ms-lg-32" src="/images/product-5.jpg"
-=======
-        class="col-12 col-lg-6 p-16 d-flex flex-column justify-content-between mb-32 pic-1"
-        style="height: 500px"
-      >
-        <div>
-          <h3 class="">凝膠指甲</h3>
-          <p class="">123456</p>
-        </div>
-        <button type="button" class="btn btn-lg btn-outline-primary align-self-end">MORE</button>
-      </div>
-      <div class="col-lg-6 d-none d-lg-block">
-        <img
-          src="https://lh3.googleusercontent.com/CpUNKN_2tD8xM8CIU1RRuSgzsJ4VDJ7NNVRh53pbYrGUJqXnYSW_BIjGaWLh91gTnru6l6NQ1VsEvURfR5w6uMYdyKd8Ujlv5gRya1YnPnz3nf4AjnyvVxnblGqXJBDbYMVKEo-h7A=w2400"
->>>>>>> origin/main
           alt=""
         />
       </div>
       <!-- 2 -->
       <!-- 3 -->
       <div class="col-lg-6 d-none d-lg-block">
-<<<<<<< HEAD
         <img src="/images/product-2.jpg"
-=======
-        <img
-          src="https://lh3.googleusercontent.com/CpUNKN_2tD8xM8CIU1RRuSgzsJ4VDJ7NNVRh53pbYrGUJqXnYSW_BIjGaWLh91gTnru6l6NQ1VsEvURfR5w6uMYdyKd8Ujlv5gRya1YnPnz3nf4AjnyvVxnblGqXJBDbYMVKEo-h7A=w2400"
->>>>>>> origin/main
           alt=""
         />
       </div>
       <div
-<<<<<<< HEAD
         class="col-12 col-lg-6 p-16 px-lg-32 d-flex flex-column justify-content-between mb-32 pic-3"
         style="height: 500px"
       >
@@ -99,21 +58,10 @@
           <p class="">現今社會有多上班族，需要長時間坐在電腦前或維持同一姿勢，可能會導致手足肌肉緊繃，造成不適。想減緩手足的壓力，保養相當重要。專業美容師可以透過精油按摩和經脈梳理，促進血液循環，有效放鬆肌肉緊張，增強手足的活力和耐力，讓您在工作中更加精力充沛。</p>
         </div>
         <button type="button" class="mb-md-32 btn btn-lg btn-outline-primary align-self-end">MORE</button>
-=======
-        class="col-12 col-lg-6 p-16 d-flex flex-column justify-content-between mb-32 pic-1"
-        style="height: 500px"
-      >
-        <div>
-          <h3 class="">沾粉指甲</h3>
-          <p class="">123456</p>
-        </div>
-        <button type="button" class="btn btn-lg btn-outline-primary align-self-end">MORE</button>
->>>>>>> origin/main
       </div>
       <!-- 3 -->
       <!-- 4 -->
       <div
-<<<<<<< HEAD
         class="col-12 col-lg-6 p-16 px-lg-32 d-flex flex-column justify-content-between mb-32 pic-4"
         style="height: 500px"
       >
@@ -125,20 +73,6 @@
       </div>
       <div class="col-lg-6 d-none d-lg-block">
         <img class="ms-lg-40" src="/images/product-3.jpg"
-=======
-        class="col-12 col-lg-6 p-16 d-flex flex-column justify-content-between mb-32 pic-1"
-        style="height: 500px"
-      >
-        <div>
-          <h3 class="">凝膠指甲</h3>
-          <p class="">123456</p>
-        </div>
-        <button type="button" class="btn btn-lg btn-outline-primary align-self-end">MORE</button>
-      </div>
-      <div class="col-lg-6 d-none d-lg-block">
-        <img
-          src="https://lh3.googleusercontent.com/CpUNKN_2tD8xM8CIU1RRuSgzsJ4VDJ7NNVRh53pbYrGUJqXnYSW_BIjGaWLh91gTnru6l6NQ1VsEvURfR5w6uMYdyKd8Ujlv5gRya1YnPnz3nf4AjnyvVxnblGqXJBDbYMVKEo-h7A=w2400"
->>>>>>> origin/main
           alt=""
         />
       </div>
@@ -154,7 +88,6 @@ img {
   object-fit: cover;
   height: 500px;
 }
-<<<<<<< HEAD
 
 .pic-1 {
   background-image: url('/images/product-4.jpg');
@@ -169,10 +102,6 @@ img {
   background-image: url('/images/product-3.jpg');
 }
 .pic-1, .pic-2, .pic-3, .pic-4 {
-=======
-.pic-1 {
-  background-image: url('https://lh3.googleusercontent.com/CpUNKN_2tD8xM8CIU1RRuSgzsJ4VDJ7NNVRh53pbYrGUJqXnYSW_BIjGaWLh91gTnru6l6NQ1VsEvURfR5w6uMYdyKd8Ujlv5gRya1YnPnz3nf4AjnyvVxnblGqXJBDbYMVKEo-h7A=w2400');
->>>>>>> origin/main
   background-position: center;
   background-size: cover;
   object-fit: cover;
